70f2f4c21372550bdf4ed6b3948c7b3b7d965b1e
fikribijikurma <auliyafikrirahmadani@gmail.com>
2026-04-20 14:19:41 +0700

    feat: implement User entity model

---
+++ src/main/java/com/example/Pertemuan7/model/User.java
@@ -0,0 +1,27 @@
+package com.example.Pertemuan7.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@Table(name = "20230140063_users")
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class User {
+    @Id
+    private String id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false)
+    private String nim;
+}
